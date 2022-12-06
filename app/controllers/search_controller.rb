319d2e1c949342cb055e77194cd84d09c18ede2c
Andre <andre@vdbapp.com>
2022-12-06 14:55:56 +0100

    refactor saved_search?

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,20 @@
 class SearchController < ApplicationController
+
   def search
-    should_save = ActiveModel::Type::Boolean.new.cast(params[:save])
     @articles = Article.where("title like ?", "%#{params[:query]}%")
 
-    if should_save && params[:query].length > 3
-      
+    if should_save?
+      SavedSearch.create(user_id: current_user.id, search_params: params[:query])
     end
 
     respond_to do |format| 
       format.js { render :search }
     end
+  end
+
+  private
+
+  def should_save?
+    ActiveModel::Type::Boolean.new.cast(params[:save]) && params[:query].length > 3 && current_user.present?
   end
 end
