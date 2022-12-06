aa439130c0bb218eab5d00a8b8357d48aba20d6b
Andre <andre@vdbapp.com>
2022-12-06 20:23:31 +0100

    install testing dependencies

--- Gemfile
+++ Gemfile
@@ -50,6 +50,9 @@ group :test do
   gem 'selenium-webdriver', '>= 4.0.0.rc1'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'rspec-rails'
+  gem 'database_cleaner'
+  gem 'factory_bot_rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
@@ -58,3 +61,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # Added Gems
 
 gem 'devise', '~> 4.8', '>= 4.8.1'
+gem 'faker', '~> 3.0'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context "when validating presence" do 
+    let(:user) { build(:user) }
+    context "when email is blank" do 
+      it "User email can't be blank" do
+        user.email = nil
+        user.valid?
+        expect(user.errors.full_messages).to include("Email can't be blank") 
+      end
+    end 
+
+    context "when password is blank" do 
+      let(:user) { User.new()}
+      it "User password can not be blank" do 
+        user.password_digest = nil,
+        user.valid?
+        expect(user.errors.full_messages).to include("Password can't be blank")
+      end 
+    end 
+  end 
+
+  context "when validating email uniqueness" do 
+    let(:user_one) { create(:user) }
+    it "user email has already been taken" do 
+      user_two = build(:user, email: user_one.email)
+      user_two.valid?
+      expect(user_two.errors.full_messages).to include("Email has already been taken")
+    end 
+  end
+
+  context "when validating password confirmation" do
+    let(:user) { build(:user) }
+    context "when passord matches password_confirmation" do 
+      it "user should be valid" do 
+        user.password_confirmation = user.password
+        expect(user).to be_valid
+      end 
+    end 
+    context "when password does not match password_confirmation" do 
+      it "user should not be valid" do 
+        user.password_confirmation = "anytypeoftext"
+        expect(user).not_to be_valid
+      end
+    end 
+  end 
+end 
