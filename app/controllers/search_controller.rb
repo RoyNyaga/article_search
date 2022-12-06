82842314abf027526632238814160c241580761a
Andre <andre@vdbapp.com>
2022-12-06 18:36:14 +0100

    add saved_search_exist to search api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     @articles = Article.where("lower(title) like ?", "%#{params[:query].downcase}%")
     @saved_searches = current_user.saved_searches
     if should_save?
-      SavedSearch.create(user_id: current_user.id, search_params: params[:query].downcase) if current_user.saved_search_exist?(params[:query].downcase)
+      SavedSearch.create(user_id: current_user.id, search_params: params[:query].downcase) unless current_user.saved_search_exist?(params[:query].downcase)
     end
     
     respond_to do |format| 
