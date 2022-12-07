6cf450a9115dbddcb4dfec3a3a3ceeecef8bedeb
Andre <andre@vdbapp.com>
2022-12-07 13:13:43 +0100

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+50.times { Article.create(title: Faker::Lorem.sentence(word_count: 5), content:  Faker::Lorem.paragraph(sentence_count: 100))}
